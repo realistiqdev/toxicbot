268c53cabb3ca58f3e8865f4c478d35cac3b3b3f
realistiqdev <ominousmatt@outlook.com>
2018-04-03 18:34:34 +0100

    Updated to a Rich Embed

--- commands/help.js
+++ commands/help.js
@@ -3,12 +3,21 @@ const ping = require("mc-hermes");
 
 //TODO: Test Ping
 module.exports.run = async (bot, message, args) => {
-  var embed=Discord.Embed(title="Help", description="A list of commands", color=0xe9dd52);
-  embed.add_field(name=!mute, value=, inline=True);
-  embed.add_field(name=!unmute, value=, inline=True);
-  embed.add_field(name=!ip, value=, inline=True);
-  embed.add_field(name=!purge, value=, inline=True);
-  message.channel.sendEmbed(embed);
+  // var embed=Discord.Embed(title="Help", description="A list of commands", color=0xe9dd52);
+  // embed.add_field(name=!mute, value=, inline=True);
+  // embed.add_field(name=!unmute, value=, inline=True);
+  // embed.add_field(name=!ip, value=, inline=True);
+  // embed.add_field(name=!purge, value=, inline=True);
+  // message.channel.sendEmbed(embed);
+  var embed = new Discord.RichEmbed()
+  .setTitle("Help")
+  .setDescription("A list of commands")
+  .setColor(0xe9dd52)
+  .addField("!mute", "", true)
+  .addField("!unmute", "", true)
+  .addField("!ip", "", true)
+  .addField("!purge", "", true);
+  message.channel.sendEmbed(embed)
 }
 
 module.exports.help = {
