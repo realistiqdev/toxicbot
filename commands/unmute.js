c0795c81e08189793cea179c1085e76e470ad6ef
realistiqdev <ominousmatt@outlook.com>
2018-04-03 17:56:32 +0100

    Added Unmute

---
+++ commands/unmute.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+  let member = message.mentions.members.first();
+  if(!member) return message.reply("Please specify a member.");
+  let muteRole = message.guild.roles.find("name", "Muted");
+  if(!muteRole) return message.reply("You don't have a role by the name of \`Muted\`.");
+  member.removeRole(muteRole.id);
+  message.channel.send(`${member.user.tag} has been unmuted.`);
+}
+
+module.exports.help = {
+  name: "unmute"
+}
