6ab76bbceeb8f1e7da6124217cfb0dcc20a70bcd
realistiqdev <ominousmatt@outlook.com>
2018-04-03 18:19:49 +0100

    Updated Dir

--- commands/help.js
+++ commands/help.js
@@ -3,13 +3,13 @@ const ping = require("mc-hermes");
 
 //TODO: Test Ping
 module.exports.run = async (bot, message, args) => {
-  fs.readdir("./", (err, files) => {
+  fs.readdir("./commands/", (err, files) => {
     if(err) console.log(err);
 
     let jsfiles = files.filter(f => f.split(".").pop() === "js")
 
     jsfiles.forEach((f, i) =>{
-      let props = require(`./${f}`);
+      let props = require(`./commands/${f}`);
       message.channel.send(`\`!${props.help.name}\` - \`${props.help.description}\``)
     });
   });
