83fa4191adce1570a191bfc5970cfb5029c5f4f2
realistiqdev <ominousmatt@outlook.com>
2018-04-03 16:24:37 +0100

    Added 'IP' command

---
+++ commands/ip.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+const ping = require("mc-hermes");
+
+module.exports.run = async (bot, message, args) => {
+  ping({
+    type: 'pc',
+    server: 'play.toxicmc.gq'
+})
+    .then((data)=>{
+        message.channel.send(`Connect using \`play.toxicmc.gq\` \nOnline players: ${data.players.online}`);
+    })
+    .catch(console.error);
+}
+
+module.exports.help = {
+  name: "ip"
+}
